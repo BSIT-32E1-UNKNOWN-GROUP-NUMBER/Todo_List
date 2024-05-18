11b897267de76884cb718315a386797ed735fc51
John gabriel rojas BSIT-32E1 <155618686+JOHNGABRIELROJAS@users.noreply.github.com>
2024-05-18 22:05:48 +0800

    creating dashboard file

---
+++ Todo-List/Todo-List/Views/Shared/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Todo_List.Views.Shared
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
